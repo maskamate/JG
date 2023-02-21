a0fc46041267dda834d13639b58bcd4c7e54ec51
Mate Maska <maska.mate.2021e@ms.gdszeged.hu>
2023-02-21 13:29:30 +0100

    2023 02 21

--- Python/Py/tengerszint.py
+++ Python/Py/tengerszint.py
@@ -1,36 +1,29 @@
-'''
-
-Írjon programot tengerszint néven!
-Kérjen be addig földrajzi hely nevét és tengerszint feletti magasságát, 
-amíg üres karaktert nem üt a felhasználó!
-
-Írjon függvényt!
-a tengerszintek néven, amely alföldet,"dombságot,"középhegység" és "magashegység" különböztet meg!
-200m alatt alföldet,
-200m és 500m között dombságot
-500m és 1500m között középhegység
-1500m magashegység
+konyvtar = input("Kérlek add meg a könyvtár nevét! ")
+könyvek = int (input("Kérlek add meg hány könyved van! (ezer) "))
 
 
-# Fájl írása tenger.txt néven az amelyben az összes eredmény szerepel!
+while konyvtar != "" or konyvtar != "":    
+    if könyvek >= 800:
+            print("A(z)", konyvtar, "ügyes gyűjtő címet kap.")
 
-Beadandó
+    elif könyvek < 800:
+            print("A(z)", konyvtar, "kevésbé szorgalmas címet kap.")
 
-A program forráskódja .py kiterjesztéssel a Githubra!
-A program forráskódja .txt kiterjesztéssel dkt-ra!
 
-'''
 
-foldr = input("Kérem adja meg a földrajzi helyet: ")
-tengersz = int(input("Kérem adja meg a tengerszintet: "))
 
-print(f"A tengerszint: {tengersz}, {foldr}-ban(-ben)")
 
-if tengersz < 200:
-    print("Alfold")
-elif tengersz >200 and 500:
-    print("Dombság")
-elif tengersz >500 and 1500:
-    print("Hegység")
-else:
-    print("Magashegység")
+
+
+
+
+
+
+
+
+
+
+
+
+
+ű
